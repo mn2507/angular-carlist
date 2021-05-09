3f1e15f09fe57ceb8e7e2a5215c8ac529941bba6
Muthu Narayanan <muthu.narayanan.hexainitiative.com>
2021-05-09 22:03:24 +0800

    Added google webview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,11 @@ import { AuthComponent } from './auth/auth.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { ChangePasswordComponent } from './auth/change-password/change-password.component';
 import { MyprofileComponent } from './myprofile/myprofile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   {
     path: 'products',
     component: RecipesComponent,
